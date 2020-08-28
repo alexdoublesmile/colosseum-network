4d0d142efa64a6628a327f5bbc80fc910d564dc5
Alex Plohoy <alexplohoy@mail.ru>
2020-08-28 09:01:40 +0300

    initial

---
+++ src/test/java/edu/plohoy/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.plohoy.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
