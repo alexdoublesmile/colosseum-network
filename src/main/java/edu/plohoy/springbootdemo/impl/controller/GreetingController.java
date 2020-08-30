3dbf18260e09f1ce1fbaf473a871483a5b9f398f
Alex Plohoy <alexplohoy@mail.ru>
2020-08-30 12:30:56 +0300

    added parameters

--- src/main/java/edu/plohoy/springbootdemo/impl/controller/GreetingController.java
+++ src/main/java/edu/plohoy/springbootdemo/impl/controller/GreetingController.java
@@ -1,13 +1,18 @@
 package edu.plohoy.springbootdemo.impl.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String getGreeting() {
+    public String getGreeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World")
+                    String name, Model model) {
+        model.addAttribute("name", name);
         return "greeting";
     }
 }
