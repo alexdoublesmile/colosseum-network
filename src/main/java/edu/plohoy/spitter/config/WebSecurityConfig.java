eb89721396d49691af29e30527f3b16ceb6c511b
Alex Plohoy <alexplohoy@mail.ru>
2020-08-31 12:32:17 +0300

    added login & logout links

--- src/main/java/edu/plohoy/spitter/config/WebSecurityConfig.java
+++ src/main/java/edu/plohoy/spitter/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package edu.plohoy.spitter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +13,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -26,6 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .logout().permitAll();
     }
+
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
