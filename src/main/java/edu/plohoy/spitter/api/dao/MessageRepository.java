d76820b1c4491d223ab8d996968e0c762a76b1bd
Alex Plohoy <alexplohoy@mail.ru>
2020-08-31 17:03:00 +0300

    refactor MessageRepository: impl CridRepository -> impl JpaRepositoty

--- src/main/java/edu/plohoy/spitter/api/dao/MessageRepository.java
+++ src/main/java/edu/plohoy/spitter/api/dao/MessageRepository.java
@@ -1,10 +1,10 @@
 package edu.plohoy.spitter.api.dao;
 
 import edu.plohoy.spitter.api.domain.Message;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MessageRepository extends CrudRepository<Message, Long> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByTag(String tag);
 }
