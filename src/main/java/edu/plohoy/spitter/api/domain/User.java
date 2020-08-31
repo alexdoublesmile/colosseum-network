89d95056d005033b13194d60bf65eeca7edbacf5
Alex Plohoy <alexplohoy@mail.ru>
2020-08-31 15:04:17 +0300

    fix user domain: userName -> username

--- src/main/java/edu/plohoy/spitter/api/domain/User.java
+++ src/main/java/edu/plohoy/spitter/api/domain/User.java
@@ -10,7 +10,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String userName;
+    private String username;
     private String password;
     private boolean active;
 
@@ -27,12 +27,12 @@ public class User {
         this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
