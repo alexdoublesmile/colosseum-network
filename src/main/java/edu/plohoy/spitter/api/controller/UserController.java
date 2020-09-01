6340630dbbab8b3635792bd35703599ee7b32355
Alex Plohoy <alexplohoy@mail.ru>
2020-09-01 07:54:10 +0300

    add userEdit controller

--- src/main/java/edu/plohoy/spitter/api/controller/UserController.java
+++ src/main/java/edu/plohoy/spitter/api/controller/UserController.java
@@ -1,10 +1,12 @@
 package edu.plohoy.spitter.api.controller;
 
 import edu.plohoy.spitter.api.dao.UserRepository;
+import edu.plohoy.spitter.api.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +20,12 @@ public class UserController {
         model.addAttribute("users", repo.findAll());
 
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+
+        return "userEdit";
     }
 }
