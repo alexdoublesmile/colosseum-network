3f8999855e1aa8ce6ef733ffb4d2a32e717ef1c7
Alex Plohoy <alexplohoy@mail.ru>
2020-09-01 08:46:26 +0300

    add preAuthorize to config

--- src/main/java/edu/plohoy/spitter/config/WebSecurityConfig.java
+++ src/main/java/edu/plohoy/spitter/config/WebSecurityConfig.java
@@ -4,18 +4,17 @@ import edu.plohoy.spitter.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-import javax.annotation.Resource;
-
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Resource(name = "user service")
+    @Autowired
     private UserService service;
 
     @Override
