bff00aa68b4ebbcc9fea978b1e08241605605bf4
Alex Plohoy <alexplohoy@mail.ru>
2020-09-02 19:24:22 +0300

    add reg validation controller

--- src/main/java/edu/plohoy/spitter/controller/RegistrationController.java
+++ src/main/java/edu/plohoy/spitter/controller/RegistrationController.java
@@ -2,13 +2,16 @@ package edu.plohoy.spitter.controller;
 
 import edu.plohoy.spitter.domain.User;
 import edu.plohoy.spitter.service.UserService;
+import edu.plohoy.spitter.utils.ControllerUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -22,9 +25,19 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
+    public String addUser(
+            @Valid User user, BindingResult bindingResult, Model model) {
+        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+            model.addAttribute("passwordError", "password are different!");
+        }
+
+        if (bindingResult.hasErrors()) {
+            model.mergeAttributes(ControllerUtils.getErrors(bindingResult));
+            return "registration";
+        }
+
         if (!service.addUser(user)) {
-            model.put("message", "User is already exists!");
+            model.addAttribute("usernameError", "User is already exists!");
             return "registration";
         }
 
