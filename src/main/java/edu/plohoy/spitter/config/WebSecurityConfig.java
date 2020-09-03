567e8219a1c6d30387f99db118586feeb86bed6f
Alex Plohoy <alexplohoy@mail.ru>
2020-09-03 07:05:27 +0300

    rest bean added

--- src/main/java/edu/plohoy/spitter/config/WebSecurityConfig.java
+++ src/main/java/edu/plohoy/spitter/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +28,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(8);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return  new RestTemplate();
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
