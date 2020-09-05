a05c5e0a15360708d3e6524e0d7bd12965d4d936
Alex Plohoy <alexplohoy@mail.ru>
2020-09-05 20:49:05 +0300

    refactor @Bean encrypt WebSecurityConfig -> EncryptionConfig

--- src/main/java/edu/plohoy/colosseum/config/WebSecurityConfig.java
+++ src/main/java/edu/plohoy/colosseum/config/WebSecurityConfig.java
@@ -22,11 +22,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder(8);
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
