6c07623658b561779420f3a413ca65f813565b71
Alex Plohoy <alexplohoy@mail.ru>
2020-09-05 20:59:56 +0300

    add hostname to UserService

--- src/main/java/edu/plohoy/colosseum/service/UserService.java
+++ src/main/java/edu/plohoy/colosseum/service/UserService.java
@@ -4,6 +4,7 @@ import edu.plohoy.colosseum.domain.Role;
 import edu.plohoy.colosseum.domain.User;
 import edu.plohoy.colosseum.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -24,6 +25,9 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Value("${hostname}")
+    private String hostname;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -102,8 +106,10 @@ public class UserService implements UserDetailsService {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s! \n" +
-                            "Welcome to Spitter. Please, visit next link: http://localhost:8080/activate/%s",
-                    user.getUsername(), user.getActivationCode()
+                            "Welcome to Spitter. Please, visit next link: http://%s/activate/%s",
+                    user.getUsername(),
+                    hostname,
+                    user.getActivationCode()
             );
             mailSender.send(user.getEmail(), "Activation Code", message);
         }
