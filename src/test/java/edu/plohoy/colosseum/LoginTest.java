6e7b454c343876d080dd74637cafc35d76e631cf
Alex Plohoy <alexplohoy@mail.ru>
2020-09-06 15:04:00 +0300

    add loginTest

--- src/test/java/edu/plohoy/colosseum/LoginTest.java
+++ src/test/java/edu/plohoy/colosseum/LoginTest.java
@@ -12,8 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,6 +33,9 @@ public class LoginTest {
 
     @Test
     public void loginTest() throws Exception {
-        
+        mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
     }
 }
