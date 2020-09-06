0b3750383cf5b49d25ae6467b0d82650e709dca4
Alex Plohoy <alexplohoy@mail.ru>
2020-09-06 15:16:47 +0300

    add badCredentialsTest

--- src/test/java/edu/plohoy/colosseum/LoginTest.java
+++ src/test/java/edu/plohoy/colosseum/LoginTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,7 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class LoginTest {
-
     @Autowired
     private MockMvc mockMvc;
 
@@ -46,5 +46,12 @@ public class LoginTest {
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void badCredentialsTest() throws Exception {
+        mockMvc.perform(post("/login").param("user", "Alfred"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
     }
 }
