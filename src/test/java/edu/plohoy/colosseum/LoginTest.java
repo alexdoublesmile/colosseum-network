500b34852466bb787e9bac575561fdea15cbec59
Alex Plohoy <alexplohoy@mail.ru>
2020-09-06 17:14:15 +0300

    fix LoginTest by adding test DB & SQLs

--- src/test/java/edu/plohoy/colosseum/LoginTest.java
+++ src/test/java/edu/plohoy/colosseum/LoginTest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
 public class LoginTest {
     @Autowired
     private MockMvc mockMvc;
@@ -41,6 +44,8 @@ public class LoginTest {
     }
 
     @Test
+    @Sql(value = "/create-user-before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/create-user-after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void correctLoginTest() throws Exception {
         mockMvc.perform(formLogin().user("admin").password("1"))
                 .andDo(print())
