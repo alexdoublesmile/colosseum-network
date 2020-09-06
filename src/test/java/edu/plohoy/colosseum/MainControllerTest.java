61dae2fda7fb9b10eb60cc93543f7e8a28aae7dc
Alex Plohoy <alexplohoy@mail.ru>
2020-09-06 17:52:15 +0300

    add addMessageToListTest

--- src/test/java/edu/plohoy/colosseum/MainControllerTest.java
+++ src/test/java/edu/plohoy/colosseum/MainControllerTest.java
@@ -11,9 +11,12 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
@@ -55,5 +58,22 @@ public class MainControllerTest {
                 .andExpect(xpath("//div[@id='message-list']/div").nodeCount(2))
                 .andExpect(xpath("//div[@id='message-list']/div[@data-id=1]").exists())
                 .andExpect(xpath("//div[@id='message-list']/div[@data-id=3]").exists());
+    }
+
+    @Test
+    public void addMessageToListTest() throws Exception {
+        MockHttpServletRequestBuilder multipart = multipart("/main")
+                .file("file", "123".getBytes())
+                .param("text", "fifth")
+                .param("tag", "new one")
+                .with(csrf());
+
+        mockMvc.perform(multipart)
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(xpath("//div[@id='message-list']/div").nodeCount(5))
+                .andExpect(xpath("//div[@id='message-list']/div[@data-id=10]").exists())
+                .andExpect(xpath("//div[@id='message-list']/div[@data-id=10]/div/span").string("fifth"))
+                .andExpect(xpath("//div[@id='message-list']/div[@data-id=10]/div/i").string("#new one"));
     }
 }
