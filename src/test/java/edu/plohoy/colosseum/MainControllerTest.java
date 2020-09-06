9c68deb3798099585aab637e31b88e6abf30eb5a
Alex Plohoy <alexplohoy@mail.ru>
2020-09-06 16:33:05 +0300

    add @Sql for testing

--- src/test/java/edu/plohoy/colosseum/MainControllerTest.java
+++ src/test/java/edu/plohoy/colosseum/MainControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @WithUserDetails("admin")
 @TestPropertySource("/application-test.properties")
+@Sql(value = "")
 public class MainControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -34,5 +36,13 @@ public class MainControllerTest {
                 .andDo(print())
                 .andExpect(authenticated())
                 .andExpect(xpath("//div[@id='navbarSupportedContent']/div").string("admin"));
+    }
+
+    @Test
+    public void messageListTest() throws Exception {
+        mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(xpath("").nodeCount(0));
     }
 }
