549d0a3d3345ce6732b92152d6732b1529adc809
Alex Plohoy <alexplohoy@mail.ru>
2020-09-06 18:22:44 +0300

    refactor MessageRepo: add Pageable to methods

--- src/main/java/edu/plohoy/colosseum/repos/MessageRepo.java
+++ src/main/java/edu/plohoy/colosseum/repos/MessageRepo.java
@@ -1,12 +1,13 @@
 package edu.plohoy.colosseum.repos;
 
 import edu.plohoy.colosseum.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
-    List<Message> findByTag(String tag);
+    Page<Message> findByTag(String tag, Pageable pageable);
+    Page<Message> findAll(Pageable pageable);
 
 }
