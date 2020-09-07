f8d265ec07feb39623280a96bc75109f90ca59b5
Alex Plohoy <alexplohoy@mail.ru>
2020-09-07 15:45:09 +0300

    add util.RedirectInterceptor class for Turbolink

---
+++ src/main/java/edu/plohoy/colosseum/util/RedirectInterceptor.java
@@ -0,0 +1,6 @@
+package edu.plohoy.colosseum.util;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class RedirectInterceptor extends HandlerInterceptorAdapter {
+}
