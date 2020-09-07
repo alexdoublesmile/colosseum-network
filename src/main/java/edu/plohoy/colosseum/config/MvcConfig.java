24c2e0ed1a075fc416812b04af16a0f7e52203bb
Alex Plohoy <alexplohoy@mail.ru>
2020-09-07 15:50:49 +0300

    add to MvcConfig override addInterceptor()

--- src/main/java/edu/plohoy/colosseum/config/MvcConfig.java
+++ src/main/java/edu/plohoy/colosseum/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package edu.plohoy.colosseum.config;
 
+import edu.plohoy.colosseum.util.RedirectInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,5 +30,10 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("file://" + uploadPath + "/");
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RedirectInterceptor());
     }
 }
