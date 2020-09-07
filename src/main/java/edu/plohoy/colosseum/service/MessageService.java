ee645151cb2243478f9aba45acb03839146d42da
Alex Plohoy <alexplohoy@mail.ru>
2020-09-07 14:45:39 +0300

    fix MessageService Message -> MessageDto & params

--- src/main/java/edu/plohoy/colosseum/service/MessageService.java
+++ src/main/java/edu/plohoy/colosseum/service/MessageService.java
@@ -2,6 +2,7 @@ package edu.plohoy.colosseum.service;
 
 import edu.plohoy.colosseum.domain.Message;
 import edu.plohoy.colosseum.domain.User;
+import edu.plohoy.colosseum.domain.dto.MessageDto;
 import edu.plohoy.colosseum.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,7 +17,7 @@ public class MessageService {
     @Autowired
     private MessageRepo dao;
 
-    public Page<Message> getMessageList(Pageable pageable, String filter, User currentUser) {
+    public Page<MessageDto> getMessageList(Pageable pageable, String filter, User currentUser) {
         if (filter != null && !filter.isEmpty()) {
             return dao.findByTag(pageable, filter, currentUser);
 
@@ -25,8 +26,8 @@ public class MessageService {
         }
     }
 
-    public Page<Message> getMessageListForUser(Pageable pageable, User currentUser, User author) {
-        return dao.findByUser(pageable, author);
+    public Page<MessageDto> getMessageListForUser(Pageable pageable, User author, User currentUser) {
+        return dao.findByUser(pageable, author, currentUser);
     }
 
     public void save(Message message) {
