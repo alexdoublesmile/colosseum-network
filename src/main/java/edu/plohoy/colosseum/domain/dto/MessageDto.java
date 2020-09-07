37ad6e8424ff9283600b65597356678a521df190
Alex Plohoy <alexplohoy@mail.ru>
2020-09-07 13:05:11 +0300

    add to MessageDTO fields, getters & constructor

--- src/main/java/edu/plohoy/colosseum/domain/dto/MessageDto.java
+++ src/main/java/edu/plohoy/colosseum/domain/dto/MessageDto.java
@@ -1,4 +1,52 @@
 package edu.plohoy.colosseum.domain.dto;
 
+import edu.plohoy.colosseum.domain.Message;
+import edu.plohoy.colosseum.domain.User;
+
 public class MessageDto {
+    private Long id;
+    private String text;
+    private String tag;
+    private String filename;
+    private User author;
+    private Long likes;
+    private Boolean meLiked;
+
+    public MessageDto(Message message, Long likes, Boolean meLiked) {
+        this.id = message.getId();
+        this.text = message.getText();
+        this.tag = message.getTag();
+        this.filename = message.getFilename();
+        this.author = message.getAuthor();
+        this.likes = likes;
+        this.meLiked = meLiked;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Long getLikes() {
+        return likes;
+    }
+
+    public Boolean getMeLiked() {
+        return meLiked;
+    }
 }
