78f2348d8560090feb87d78382577a5d0c892fc7
Александр Плохой <plohoy@zen-it.ru>
2021-11-04 05:56:47 +0300

    fix: add params to picture query

--- src/main/java/edu/plohoy/colosseum/controller/PictureController.java
+++ src/main/java/edu/plohoy/colosseum/controller/PictureController.java
@@ -1,5 +1,6 @@
 package edu.plohoy.colosseum.controller;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PictureController {
 
     @GetMapping("/9910000037938419")
-    public String myPicture() {
+    public String myPicture(@Param(value = "lang") String lang, @Param(value = "ck")String ck) {
         return "plohoy_alexandr";
     }
 
     @GetMapping("/9910000037938429")
-    public String catherinePicture() {
+    public String catherinePicture(@Param(value = "lang") String lang, @Param(value = "ck")String ck) {
         return "catherine";
     }
 }
